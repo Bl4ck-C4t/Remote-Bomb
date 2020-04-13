893301de23916cc701aaaa8c7889ec46be7f0b9c
TheShadow88 <stef4oben88@gmail.com>
2020-04-13 17:09:13 +0300

    [feat] Added counter, keypad and more

--- receive.py
+++ receive.py
@@ -1,19 +1,111 @@
 import socket
 from struct import unpack_from
 import json
+import time
+import asyncio
+import threading
 
-BOMB_PROT_ID = 0xC4C4
-s1 = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
-print("Listening for bomb frames...")
-while True:
-    all_data = s1.recv(80)
 
-    layer2 = unpack_from("!6B6BH", all_data)
-    if layer2[-1] != 0xc4c4:
-        continue
-    print("Received commands!")
-    print(all_data)
-    commands = json.loads(all_data[14:].decode())
-    print(commands)
+class Bomb:
+    def __init__(self):
+        self._ticking = False
+        self.counter_thread = None
+        self.requestThread = None
+        self.sensorThread = None
+        self.keypadThread = None
+        self.timer = 0
+        self.modifier = 1
 
-    print("Listening for bomb frames...")
+        self.startListening()
+        self.keypadListen()
+        self.sensorListen()
+
+    def terminate(self):
+        self._ticking = False
+
+    def activateCounter(self):
+        self._ticking = True
+        self.counter_thread = threading.Thread(target=self.countdown, args=(2 * 60,))
+        self.counter_thread.start()
+
+    def startListening(self):
+        self.requestThread = threading.Thread(target=self.onRequestReceived)
+        self.requestThread.start()
+
+    def sensorListen(self):
+        self.sensorThread = threading.Thread(target=self.onSensorDetectsMovement)
+        self.sensorThread.start()
+
+    def keypadListen(self):
+        self.keypadThread = threading.Thread(target=self.onKeypadSubmit)
+        self.keypadThread.start()
+
+    def countdown(self, t):
+        self.timer = t
+        while self.timer and self._ticking:
+            mins, secs = divmod(self.timer, 60)
+            timeformat = f'{mins}:{secs}'
+            print(timeformat)
+            time.sleep(1 / self.modifier)
+            self.timer -= 1
+            self.beepSound()
+        self.detonate()
+
+    def activate(self):
+        self.activateCounter()
+
+    def onKeypadSubmit(self):
+        # get input from the keypad
+        pass
+        # if code is correct
+        #   self.terminate()
+        # else
+        #   self.modifier += 0.5
+
+    def onSensorDetectsMovement(self):
+        pass
+        # get input from the sensor
+        # if movement too much
+        #   self.detonate()
+
+    def beepSound(self):
+        # make the buzzer beep
+        pass
+
+    def onRequestReceived(self):
+
+        BOMB_PROT_ID = 0xC4C4
+        s1 = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+        print("Listening for bomb frames...")
+        while True:
+            all_data = s1.recv(80)
+
+            layer2 = unpack_from("!6B6BH", all_data)
+            if layer2[-1] != 0xc4c4:
+                continue
+            print("Received commands!")
+            # print(all_data)
+            commands = json.loads(all_data[14:].decode())
+            if 'k3y' in commands and commands['k3y'] == "valhalaa":
+                command = commands["command"]
+                if command == "defuse":
+                    self.terminate()
+                    print("Bomb defused")
+                elif command == "arm":
+                    self.activate()
+                else:
+                    print("unknown command")
+            else:
+                self.modifier += 0.5
+
+            print("Listening for bomb frames...")
+
+    def detonate(self):
+        # trigger explosion / led inidication
+        self.terminate()
+        print("Boom!")
+
+
+bomb = Bomb()
+
+exit()
--- send.py
+++ send.py
@@ -7,7 +7,7 @@ from uuid import getnode as get_mac
 rasp_wlan0 = b'\xb8\x27\xeb\x2a\x5e\xf7'
 rasp_eth0 = b'\xb8\x27\xeb\x7f\x0b\xa2'
 broadcast = b'\xff\xff\xff\xff\xff\xff'
-
+laptop = b'\x00\x0c\x29\xef\xbc\xc6'
 
 def send_commands(target_mac, data):
     interface = 'ens33'
@@ -16,13 +16,16 @@ def send_commands(target_mac, data):
     BOMB_FRAME = [
         bytes(json.dumps(data), 'utf-8')
     ]
-    print(BOMB_FRAME)
-    print(b''.join(BOMB_FRAME))
+    # print(BOMB_FRAME)
+    # print(b''.join(BOMB_FRAME))
     addr = (interface, BOMB_PROT_ID, socket.PACKET_HOST, 0x0, target_mac)
     sock.sendto(b''.join(BOMB_FRAME), addr)
     sock.close()
 
 
 local_mac = [int("{0:012x}".format(get_mac())[i:i + 2], 16) for i in range(0, 12, 2)]
-rasp = [184, 39, 235, 42, 94, 247]
-send_commands(broadcast, {'msg': "Explode"})
+print("Commands:\n'defuse' - to defuse the bomb\n'arm' - to activate it")
+while True:
+    command = input("Enter command: ")
+    send_commands(laptop, {'k3y': "valhalaa", "command": f"{command}"})
+    print("Sent commands successfully!")
