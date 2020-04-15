37995365314344b7c6ec3c00980a64e4c9d45669
TheShadow88 <stef4oben88@gmail.com>
2020-04-15 11:31:54 +0300

    [mod] Changed server thread with corotines

--- receive.py
+++ receive.py
@@ -10,12 +10,12 @@ class Bomb:
     def __init__(self):
         self._ticking = False
         self.counter_thread = None
-        self.requestThread = None
         self.sensorThread = None
         self.keypadThread = None
         self.defused = False
         self.timer = 0
         self.modifier = 1
+        self.loop = asyncio.get_event_loop()
 
         self.startListening()
         self.keypadListen()
@@ -30,8 +30,7 @@ class Bomb:
         self.counter_thread.start()
 
     def startListening(self):
-        self.requestThread = threading.Thread(target=self.onRequestReceived)
-        self.requestThread.start()
+        self.loop.run_until_complete(self.onRequestReceived())
 
     def sensorListen(self):
         self.sensorThread = threading.Thread(target=self.onSensorDetectsMovement)
@@ -74,34 +73,37 @@ class Bomb:
         # make the buzzer beep
         pass
 
-    def onRequestReceived(self):
+    async def onRequestReceived(self):
 
         BOMB_PROT_ID = 0xC4C4
         s1 = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.ntohs(0x0003))
+        s1.setblocking(False)
         print("Listening for bomb frames...")
         while True:
-            all_data = s1.recv(80)
+            all_data = await self.loop.sock_recv(s1, 80)
+            self.loop.create_task(self.handleRequest(all_data))
 
-            layer2 = unpack_from("!6B6BH", all_data)
-            if layer2[-1] != 0xc4c4:
-                continue
-            print("Received commands!")
-            # print(all_data)
-            commands = json.loads(all_data[14:].decode())
-            if 'k3y' in commands and commands['k3y'] == "valhalaa":
-                command = commands["command"]
-                if command == "defuse":
-                    self.defused = True
-                    self.terminate()
-                    print("Bomb defused")
-                elif command == "arm":
-                    self.activate()
-                else:
-                    print("unknown command")
+    async def handleRequest(self, all_data):
+        layer2 = unpack_from("!6B6BH", all_data)
+        if layer2[-1] != 0xc4c4:
+            return
+        print("Received commands!")
+        # print(all_data)
+        commands = json.loads(all_data[14:].decode())
+        if 'k3y' in commands and commands['k3y'] == "valhalaa":
+            command = commands["command"]
+            if command == "defuse":
+                self.defused = True
+                self.terminate()
+                print("Bomb defused")
+            elif command == "arm":
+                self.activate()
             else:
-                self.modifier += 0.5
+                print("unknown command")
+        else:
+            self.modifier += 0.5
 
-            print("Listening for bomb frames...")
+        print("Listening for bomb frames...")
 
     def detonate(self):
         # trigger explosion / led inidication
